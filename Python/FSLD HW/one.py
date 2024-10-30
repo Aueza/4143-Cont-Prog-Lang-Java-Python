bb64115fd4a747ca29751b0fc56637c8641c61a4
Ethan <134173034+Aueza@users.noreply.github.com>
2024-10-30 21:55:54 +0000

    start one.py

--- Python/FSLD HW/one.py
+++ Python/FSLD HW/one.py
@@ -6,5 +6,9 @@ with open('/workspaces/4143-Cont-Prog-Lang-Java-Python/Python/FSLD HW/one.txt', 
 # declare the pattern we are searching for aka "the"
 pattern = r'\bthe\b'
 matches = re.findall(pattern, text)
+
+# for each match, increment count
+count = 0
 for match in matches:
-    print(match.group())
+    count += 1
+print(f"There are {count} 'the's")
